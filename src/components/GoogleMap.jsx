4a8b1cc8832dac3fd31e07f9e084013bbc90da57
sejal-6102 <jainsejal6102@gmail.com>
2025-04-19 11:36:36 +0530

    vercel.json

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -14,8 +14,8 @@ const GoogleMap = () => {
                   width="100%"
                   height="480px"
                   frameborder="0"
-                  marginheight="0"
-                  marginwidth="0"
+                  marginHeight="0"
+                  marginWidth="0"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3559.2392126363775!2d75.7388628752586!3d26.91502387664637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396db4642bfc12b5%3A0xeef7ca56bebad0!2sDronum%20India%20Aviations%20Pvt.%20Ltd!5e0!3m2!1sen!2sin!4v1712400000000!5m2!1sen!2sin"
                 ></iframe>
               </div>
