2ed8d00506dbb6404ba603e6d7751c45b0b1848a
sejal-6102 <jainsejal6102@gmail.com>
2025-05-02 17:14:42 +0530

    changes8

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const adminDataRoutes = require("./routes/adminData");
 const app = express();
 // app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://dronum.vercel.app',
     // origin: 'http://localhost:3000',   //  // <-- Frontend URL
   methods: ['GET', 'POST'], // Add others if needed: PUT, DELETE, etc.
   credentials: true // if using cookies or auth
